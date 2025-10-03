114629f0707800abd8b1037f4cb9ea0765a4f8e0
unknown <ibrahim713643420@gmail.com>
2025-10-03 14:20:08 +0300

    some methods in C# math class like max, min , abs, sqrt ..etc

---
+++ Stage2/14_C#_level1/math/math/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max of 5, 10 is: {0}", Math.Max(5, 10));
+            Console.WriteLine("Min of 5, 10 is: {0}", Math.Min(5, 10));
+            Console.WriteLine("Squir Root of 64 is: {0}", Math.Sqrt(64));
+            Console.WriteLine("Absolute (positive) value of  -4.7 is: {0}", Math.Abs(-4.7));
+            Console.WriteLine("Round of 9.99 is: {0}", Math.Round(9.99));
+
+        }
+    }
+}
